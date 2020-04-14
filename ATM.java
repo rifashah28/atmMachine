228fe8656bf91c130d76a0f0acb653c0307b955d
Rifa Safeer Shah <rifashah28@gmail.com>
2020-04-14 15:19:40 -0700

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,23 @@
+/**
+ * Description of the class
+ * ATM Machine: ATM
+ * @author Rifa Safeer Shah, Fernando Gonzales, Nathaniel Monte De Ramos.
+ * Date: 04/14/2020
+ */
+
+package atmMachine;
+
+public class ATM {
+	/**
+	 * 
+	 */
+	public void menu() {
+		System.out.println("Welcome to the ATM.");
+		System.out.println("Choose from the following Transactions or Exit.");
+		System.out.println("1 - Deposit Funds.");
+		System.out.println("2 - Withdraw Funds.");
+		System.out.println("3 - Check Balance.");
+		System.out.println("4 - Transfer Balance.");
+		System.out.println("5 - Exit.");
+	} // End of menu
+} // End of ATM class
