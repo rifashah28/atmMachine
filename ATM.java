66c835eb1e97241895c2209d0bf8563332480de4
nando2nando <60991859+nando2nando@users.noreply.github.com>
2020-04-15 14:10:43 -0700

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -9,15 +9,23 @@ package atmMachine;
 
 public class ATM {
 	/**
+	 * i added customer to the menu arguement so you can use the functions in account which are inside of customer
 	 * 
 	 */
-	public void menu() {
+	public void menu(Customer customer) {
+		
+		Withdraw withdraw = new Withdraw();
+		int choice = -1;
+		
+		
 		System.out.println("Welcome to the ATM.");
 		System.out.println("Choose from the following Transactions or Exit.");
 		System.out.println("1 - Deposit Funds.");
-		System.out.println("2 - Withdraw Funds.");
+		System.out.println("2 - Withdraw Funds.");		
 		System.out.println("3 - Check Balance.");
 		System.out.println("4 - Transfer Balance.");
 		System.out.println("5 - Exit.");
 	} // End of menu
 } // End of ATM class
+
+
