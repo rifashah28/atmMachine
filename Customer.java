6dbc51788d8cb3df887b2be40f92af53900f2ac6
nando2nando <60991859+nando2nando@users.noreply.github.com>
2020-04-15 14:11:58 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -7,6 +7,7 @@
 
 package atmMachine;
 
+
 public class Customer {
 	/** First Name of the Customer. */
 	private String firstName;
@@ -18,23 +19,30 @@ public class Customer {
 	private String pinNumber;
 	/** Balance in the Account. */
 	private double balance;
-	
+
 	/**
 	 * Constructor for the Customer class.
-	 * @param firstName is the First Name of the Customer.
-	 * @param lastName is the Last Name of the Customer.
+	 * 
+	 * @param firstName     is the First Name of the Customer.
+	 * @param lastName      is the Last Name of the Customer.
 	 * @param accountNumber is the Account Number of the Customer.
-	 * @param pinNumber is the Pin Number for the Account
+	 * @param pinNumber     is the Pin Number for the Account
 	 */
-	public Customer (String firstName, String lastName, String accountNumber, String pinNumber) {
+	public Customer(String firstName, String lastName, String accountNumber, String pinNumber) {
 		setFirstName(firstName);
 		setLastName(lastName);
 		setAccountNumber(accountNumber);
 		setPinNumber(pinNumber);
+		setBalance(100);
+		
+		
+		// here i create the account for the customer and set the amount in the account to 100$
+		Account account = new Account(accountNumber,pinNumber,balance);
 	} // End of Customer constructor
-	
+
 	/**
 	 * Sets the Pin Number for the Account.
+	 * 
 	 * @param pinNumber is the Pin Number for the Account.
 	 */
 	private void setPinNumber(String pinNumber) {
@@ -43,17 +51,19 @@ public class Customer {
 			this.pinNumber = pinNumber;
 		} // End of if-statement
 	} // End of setPinNumber
-	
+
 	/**
 	 * Gets the pin Number for the Account.
+	 * 
 	 * @return pinNumber. Pin Number as a String.
 	 */
 	public String getPinNumber() {
 		return pinNumber;
 	} // End of getPinNumber
-	
+
 	/**
 	 * Sets the Account Number of the Customer.
+	 * 
 	 * @param accountNumber is the Account Number of the Customer.
 	 */
 	private void setAccountNumber(String accountNumber) {
@@ -62,9 +72,10 @@ public class Customer {
 			this.accountNumber = accountNumber;
 		} // End of if-statement
 	} // End of setAccountNumber
-	
+
 	/**
 	 * Gets the Account Number of the Customer.
+	 * 
 	 * @return accountNumber. Account Number as a String.
 	 */
 	public String getAccountNumber() {
@@ -73,6 +84,7 @@ public class Customer {
 
 	/**
 	 * Sets the Last Name of the Customer.
+	 * 
 	 * @param lastName is the Last Name of the Customer.
 	 */
 	private void setLastName(String lastName) {
@@ -81,9 +93,10 @@ public class Customer {
 			this.lastName = lastName;
 		} // End of if-statement
 	} // End of setLastName
-	
+
 	/**
 	 * Gets the Last Name of the Customer.
+	 * 
 	 * @return lastName. Last Name as a String.
 	 */
 	public String getLastName() {
@@ -92,6 +105,7 @@ public class Customer {
 
 	/**
 	 * Sets the First Name of the Customer.
+	 * 
 	 * @param firstName is the First Name of the Customer.
 	 */
 	private void setFirstName(String firstName) {
@@ -100,17 +114,27 @@ public class Customer {
 			this.firstName = firstName;
 		} // End of if-statement
 	} // End of setFirstName
-	
+
 	/**
 	 * Gets the First Name of the Customer.
+	 * 
 	 * @return firstName. First Name as a String.
 	 */
 	public String getFirstName() {
 		return firstName;
 	} // End of getFirstName
+
+	/*====================================================
+	 * shouldnt this be in another place like under get balance atm class/function
+	 * 
+	 * 
+	 */
+	
+	
 	
 	/**
 	 * Sets the Balance in the Account.
+	 * 
 	 * @param balance is the Balance in the Account.
 	 */
 	public void setBalance(double balance) {
@@ -118,17 +142,19 @@ public class Customer {
 			this.balance = balance;
 		} // End of if-statement
 	} // End of setBalance
-	
+
 	/**
 	 * Gets the Balance in the Account.
+	 * 
 	 * @return balance. Balance as a double.
 	 */
 	public double getBalance() {
 		return balance;
 	} // End of getBalance
-	
+
 	/**
 	 * Adding amount to the Balance in the Account.
+	 * 
 	 * @param amount. Amount added to the Account.
 	 */
 	public void addToBalance(double amount) {
@@ -136,9 +162,10 @@ public class Customer {
 			balance = balance + amount;
 		} // End of if-statement
 	} // End of addToBalance
-	
+
 	/**
 	 * Subtracting amount from the Balance in the Account.
+	 * 
 	 * @param amount. Amount subtracted from the Account.
 	 */
 	public void subtractFromBalance(double amount) {
@@ -147,3 +174,6 @@ public class Customer {
 		} // End of if-statement
 	} // End of subtractFromBalance
 } // End of Customer class
+
+
+
