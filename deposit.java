c274a35de8d8b1240a1b22e2206c53dd0ac591e2
Rifa Safeer Shah <rifashah28@gmail.com>
2020-04-18 13:07:39 -0700

    Create deposit.java

---
+++ deposit.java
@@ -0,0 +1,11 @@
+public class Deposit {
+	public void Deposit() {
+  }
+
+	public double Deposit(Customer customer) {
+		double funds =0;
+		System.out.println("How much would you like to deposit?")?
+		System.out.println("Your balance is "+ customer.getBalance());
+		return 0;
+	}
+}
