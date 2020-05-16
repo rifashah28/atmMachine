1b3464ff01b3d0687de15907bf8ebfb1dfeaf498
Rifa Safeer Shah <rifashah28@gmail.com>
2020-05-16 14:22:30 -0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,103 +1,60 @@
 /**
- * Description: Account information for the ATM machine.
- * CECS 343 SPRING 2020
+ * Description: The account operator for the ATM machine.
+ * CECS 343 Spring 2020
  * ATM Machine: Account
  * @author Nathaniel Monte De Ramos and Rifa Safeer Shah
- * Date: 05/05/2020
+ * Date: 05/06/2020
  */
 
 package atmMachine;
 
+public class Account {
 
-
-public class Account
-
-{
-
+	/* Card number for the account */
 	static int cardNum;
 
+	/* Pin number for the account */
 	static int pinNum;
 
+	/* Balance in the account */
 	static String balance;
 
-	
-
-	public Account(int card,int pin, String bal) 
-
-	{
+	public Account(int card,int pin, String bal) {
 
 		Account.cardNum = card;
 
 		Account.pinNum = pin;
 
 		Account.balance = bal;
-
-	}
-
-	
-
-
+	} // End of Account
 
 	/**
-
-	 * Gets the Balance in the Account.
-
-	 * 
-
+	 * Gets the balance in the account.
 	 * @return balance. Balance as a double.
-
 	 */
-
-	public static double getBalance()
-
-	{
+	public static double getBalance() {
 
 		return Double.parseDouble(balance);
-
 	} // End of getBalance
 
-
-
-
-
-	
-
-	public static int getCardNum() 
-
-	{
+	/**
+	 * Gets the card number.
+	 * @return card number. Card Number as an integer.
+	 */
+	public static int getCardNum() {
 
 		return cardNum;
-
-	}
-
-
-
-
+	} // End of getCardNum
 
 	/**
-
-	 * Sets the Balance in the Account.
-
-	 * 
-
-	 * @param balance is the Balance in the Account.
-
+	 * Sets the balance in the account.
+	 * @param bal balance in the account.
 	 */
+	public static void setBalance(double bal) {
 
-	public static void setBalance(double bal) 
-
-	{
-
-		
-
-		if (Double.parseDouble(balance) >= 0)
-
-		{
+		if (Double.parseDouble(balance) >= 0) {
 
 			balance = String.valueOf(bal);
-
 		} // End of if-statement
-
 	} // End of setBalance
-
-}
+} // End of Account class
