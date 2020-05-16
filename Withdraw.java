9e3a52b1a56f11481560d17cd833bab0e45828a4
Rifa Safeer Shah <rifashah28@gmail.com>
2020-05-16 14:19:24 -0700

    Update Withdraw.java

--- Withdraw.java
+++ Withdraw.java
@@ -1,28 +1,21 @@
 /**
- * Description: Carry out the withdraw transaction in the ATM machine.
- * CECS 343 SPRING 2020
+ * Description: This class allows users to take money out of the account.
+ * CECS 343 Spring 2020
  * ATM Machine: Withdraw
  * @author Nathaniel Monte De Ramos and Rifa Safeer Shah
- * Date: 05/05/2020
+ * Date: 05/06/2020
  */
 
 package atmMachine;
 
-
-
 import java.util.Scanner;
 
+/**
+ * Subtracts an amount from the account balance.
+ */
+public class Withdraw {
 
-
-//subracts an Amount from the account balance
-
-public class Withdraw 
-
-{
-
-	public static void withdraw() 
-
-	{
+	public static void withdraw() {
 
 		double withdrawAmount;
 
@@ -33,30 +26,22 @@ public class Withdraw
 		System.out.println("===============WITHDRAW===============");
 
 		System.out.println("Your balance is " + Account.getBalance());
+		
+		/**
+		 * User gets 3 tries.
+		 */
+		for(int i = 0; i < 3; i++) {
 
-
-
-		for(int i = 0; i < 3; i++)//user gets 3 tries
-
-		{
-
-			System.out.println("How much woul you like to withdraw?");
+			System.out.println("How much would you like to withdraw?");
 
 			withdrawAmount = scan.nextDouble();
 
-			
-
-			if(withdrawAmount >= Account.getBalance()) 
-
-			{
+			if (withdrawAmount >= Account.getBalance()) {
 
 				System.out.println("Withdrawal Amount is Greater than your Balance.\nPlease Try Again.");
+			} // End of if-statement
 
-			}
-
-			else 
-
-			{
+			else {
 
 				accBalance = Account.getBalance() - withdrawAmount;
 
@@ -65,15 +50,9 @@ public class Withdraw
 				Account.setBalance(accBalance);
 
 				i += 2;
-
-			}
-
-		}
+			} // End of else-statement
+		} // End of for loop
 
 		System.out.println("======================================");
-
-	}
-
-}
-
-//DO SOME TEST TO CHECK IF ITS RIGHT
+	} // End of withdraw
+} // End of Withdraw class
